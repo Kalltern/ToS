0b478d73ea3f62054253efddb81d4668e8957405
Kalltern <jan.c.horak@gmail.com>
2022-07-19 20:47:06 +0100

    Attributetest

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -1,22 +1,27 @@
 import { EntitySheetHelper } from "./helper.js";
-import {ATTRIBUTE_TYPES} from "./constants.js";
+import { ATTRIBUTE_TYPES } from "./constants.js";
 
 /**
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
 export class SimpleActorSheet extends ActorSheet {
-
   /** @inheritdoc */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["worldbuilding", "sheet", "actor"],
-      template: "systems/worldbuilding/templates/actor-sheet.html",
+      template: "systems/tos/templates/actor-sheet.html",
       width: 600,
       height: 600,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "description",
+        },
+      ],
       scrollY: [".biography", ".items", ".attributes"],
-      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
+      dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }],
     });
   }
 
@@ -25,7 +30,6 @@ export class SimpleActorSheet extends ActorSheet {
   /** @inheritdoc */
   getData() {
     const context = super.getData();
-    EntitySheetHelper.getAttributeData(context.data);
     context.shorthand = !!game.settings.get("worldbuilding", "macroShorthand");
     context.systemData = context.data.data;
     context.dtypes = ATTRIBUTE_TYPES;
@@ -39,12 +43,7 @@ export class SimpleActorSheet extends ActorSheet {
     super.activateListeners(html);
 
     // Everything below here is only needed if the sheet is editable
-    if ( !this.isEditable ) return;
-
-    // Attribute Management
-    html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
-    html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
-    html.find(".attributes").on("click", "a.attribute-roll", EntitySheetHelper.onAttributeRoll.bind(this));
+    if (!this.isEditable) return;
 
     // Item Controls
     html.find(".item-control").click(this._onItemControl.bind(this));
@@ -53,10 +52,14 @@ export class SimpleActorSheet extends ActorSheet {
     // Add draggable for Macro creation
     html.find(".attributes a.attribute-roll").each((i, a) => {
       a.setAttribute("draggable", true);
-      a.addEventListener("dragstart", ev => {
-        let dragData = ev.currentTarget.dataset;
-        ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-      }, false);
+      a.addEventListener(
+        "dragstart",
+        (ev) => {
+          let dragData = ev.currentTarget.dataset;
+          ev.dataTransfer.setData("text/plain", JSON.stringify(dragData));
+        },
+        false
+      );
     });
   }
 
@@ -76,10 +79,13 @@ export class SimpleActorSheet extends ActorSheet {
     const item = this.actor.items.get(li?.dataset.itemId);
 
     // Handle different actions
-    switch ( button.dataset.action ) {
+    switch (button.dataset.action) {
       case "create":
         const cls = getDocumentClass("Item");
-        return cls.create({name: game.i18n.localize("SIMPLE.ItemNew"), type: "item"}, {parent: this.actor});
+        return cls.create(
+          { name: game.i18n.localize("SIMPLE.ItemNew"), type: "item" },
+          { parent: this.actor }
+        );
       case "edit":
         return item.sheet.render(true);
       case "delete":
@@ -97,11 +103,11 @@ export class SimpleActorSheet extends ActorSheet {
     let button = $(event.currentTarget);
     const li = button.parents(".item");
     const item = this.actor.items.get(li.data("itemId"));
-    let r = new Roll(button.data('roll'), this.actor.getRollData());
+    let r = new Roll(button.data("roll"), this.actor.getRollData());
     return r.toMessage({
       user: game.user.id,
       speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
+      flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`,
     });
   }
 
