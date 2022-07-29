114c66465c184bac60ddc26f98cb59f2f4241f2d
Kalltern <jan.c.horak@gmail.com>
2022-07-29 13:25:45 +0200

    Boilerplate update

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const TOS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+TOS.abilities = {
+  str: "TOS.AbilityStr",
+  dex: "TOS.AbilityDex",
+  end: "TOS.AbilityEnd",
+  int: "TOS.AbilityInt",
+  wil: "TOS.AbilityWil",
+  cha: "TOS.AbilityCha",
+  per: "TOS.AbilityPer",
+};
+
+TOS.abilityAbbreviations = {
+  str: "TOS.AbilityStrAbbr",
+  dex: "TOS.AbilityDexAbbr",
+  end: "TOS.AbilityEndAbbr",
+  int: "TOS.AbilityIntAbbr",
+  wil: "TOS.AbilityWilAbbr",
+  cha: "TOS.AbilityChaAbbr",
+  per: "TOS.AbilityPerAbbr",
+};
