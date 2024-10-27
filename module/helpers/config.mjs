f1acb840ed00841d6b9eada1c9026e18c67b697c
Kalltern <jan.c.horak@gmail.com>
2024-10-27 09:27:59 +0100

    Added Abilities
    Added Will, Constitution, Perception

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,19 +5,21 @@ export const TOS = {};
  * @type {Object}
  */
 TOS.abilities = {
-  str: 'TOS.Ability.Str.long',
-  dex: 'TOS.Ability.Dex.long',
-  con: 'TOS.Ability.Con.long',
-  int: 'TOS.Ability.Int.long',
-  wis: 'TOS.Ability.Wis.long',
-  cha: 'TOS.Ability.Cha.long',
+  str: "TOS.Ability.Str.long",
+  dex: "TOS.Ability.Dex.long",
+  end: "TOS.Ability.End.long",
+  int: "TOS.Ability.Int.long",
+  wil: "TOS.Ability.Wil.long",
+  cha: "TOS.Ability.Cha.long",
+  per: "TOS.Ability.Per.long",
 };
 
 TOS.abilityAbbreviations = {
-  str: 'TOS.Ability.Str.abbr',
-  dex: 'TOS.Ability.Dex.abbr',
-  con: 'TOS.Ability.Con.abbr',
-  int: 'TOS.Ability.Int.abbr',
-  wis: 'TOS.Ability.Wis.abbr',
-  cha: 'TOS.Ability.Cha.abbr',
+  str: "TOS.Ability.Str.abbr",
+  dex: "TOS.Ability.Dex.abbr",
+  end: "TOS.Ability.End.abbr",
+  int: "TOS.Ability.Int.abbr",
+  wil: "TOS.Ability.Wil.abbr",
+  cha: "TOS.Ability.Cha.abbr",
+  per: "TOS.Ability.Per.abbr",
 };
